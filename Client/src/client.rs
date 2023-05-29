32b79f0d4a3b3534059c95e501d229cd5939b4bb
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2023-05-29 22:30:34 +0200

    Finish

--- Client/src/client.rs
+++ Client/src/client.rs
@@ -1,23 +1,110 @@
-use std::io;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 use std::net::TcpStream;
 
-fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
-    loop {
-        let mut input = String::new();
-        println!("Enter message: ");
-        match io::stdin().read_line(&mut input) {
-            Ok(_) => {
-                let _ = stream.write(input.as_bytes());
-                let mut buf = [0; 512];
-                let bytes_read = stream.read(&mut buf).unwrap();
-                if bytes_read == 0 {
-                    return;
+struct Client {
+    stream: TcpStream,
+    account_number: String,
+}
+
+impl Client {
+    fn new(address: &str, account_number: String) -> io::Result<Self> {
+        let stream = TcpStream::connect(address)?;
+        Ok(Self {
+            stream,
+            account_number,
+        })
+    }
+
+    fn send_request(&mut self, request: &str) -> io::Result<String> {
+        self.stream.write(request.as_bytes())?;
+        
+        let mut buffer = [0; 512];
+        self.stream.read(&mut buffer)?;
+        
+        let response = String::from_utf8_lossy(&buffer[..]);
+        Ok(response.to_string())
+    }
+
+    fn handle_balance_operation(&mut self) -> io::Result<()> {
+        let out = format!("balance {}", self.account_number);
+        let response = self.send_request(&out)?;
+        println!("{}", response);
+        Ok(())
+    }
+
+    fn handle_withdraw_operation(&mut self) -> io::Result<()> {
+        let amount = read_input("Enter withdraw amount: ")?;
+        let pin = read_input("Enter pin to confirm operation: ")?;
+        let out = format!("withdraw {} {} {}", self.account_number, pin, amount);
+        let response = self.send_request(&out)?;
+        println!("{}", response);
+        Ok(())
+    }
+
+    fn handle_deposit_operation(&mut self) -> io::Result<()> {
+        let amount = read_input("Enter deposit amount: ")?;
+        let out = format!("deposit {} {}", self.account_number, amount);
+        let response = self.send_request(&out)?;
+        println!("{}", response);
+        Ok(())
+    }
+
+    fn handle_transfer_operation(&mut self) -> io::Result<()> {
+        let number2 = read_input("Enter destination account number: ")?;
+        let amount = read_input("Enter transfer amount: ")?;
+        let pin = read_input("Enter pin to confirm operation: ")?;
+        let out = format!("transfer {} {} {} {}", self.account_number, number2, amount, pin);
+        let response = self.send_request(&out)?;
+        println!("{}", response);
+        Ok(())
+    }
+
+    fn run(&mut self) -> io::Result<()> {
+        println!("Connected to server!");
+
+        loop {
+            let operation = read_input("Enter your operation (balance, withdraw, deposit, transfer): ")?;
+
+            match operation.as_str() {
+                "balance" => {
+                    println!();
+                    self.handle_balance_operation()?;
                 }
-                println!("Received: {}", String::from_utf8_lossy(&buf[0..bytes_read]));
+                "withdraw" => {
+                    println!();
+                    self.handle_withdraw_operation()?;
+                }
+                "deposit" => {
+                    println!();
+                    self.handle_deposit_operation()?;
+                }
+                "transfer" => {
+                    println!();
+                    self.handle_transfer_operation()?;
+                }
+                _ => {
+                    println!("Unknown command '{}'", operation);
+                }
             }
-            Err(error) => println!("error: {}", error),
         }
     }
+}
+
+fn read_input(prompt: &str) -> io::Result<String> {
+    print!("{}", prompt);
+    io::stdout().flush()?;
+    
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    
+    Ok(input.trim().to_string())
+}
+
+fn main() -> io::Result<()> {
+    let address = "127.0.0.1:8080";
+    let account_number = read_input("Enter your account number: ")?;
+
+    let mut client = Client::new(address, account_number)?;
+    client.run()?;
+    Ok(())
 }
--- Server/src/server.rs
+++ Server/src/server.rs
@@ -1,137 +1,201 @@
+use rusqlite::{Connection, Result};
 use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-#[derive(Debug)]
-struct Client {
-    first_name: String,
-    last_name: String,
+const SAVE_PATH: &str = "data/customers.db";
+
+struct Customer {
+    name: String,
+    surname: String,
     pesel: String,
     account_number: String,
+    pin: String,
     balance: f64,
 }
 
-impl Client {
-    fn new(
-        first_name: String,
-        last_name: String,
-        pesel: String,
-        account_number: String,
-        balance: f64,
-    ) -> Self {
-        Self {
-            first_name,
-            last_name,
-            pesel,
-            account_number,
-            balance,
-        }
+// impl Customer {
+//     fn new(name: &str, surname: &str, pesel: &str, account_number: &str, pin: &str, balance: f64) -> Customer {
+//         Customer {
+//             name: name.to_string(),
+//             surname: surname.to_string(),
+//             pesel: pesel.to_string(),
+//             account_number: account_number.to_string(),
+//             pin: pin.to_string(),
+//             balance,
+//         }
+//     }
+// }
+
+fn write_customers_to_database(
+    customers: &[Customer],
+    db_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut conn = Connection::open(db_path)?;
+
+    conn.execute("DELETE FROM customers", [])?; // Clear existing data
+
+    let tx = conn.transaction()?;
+    let mut statement = tx.prepare(
+        "INSERT INTO customers (name, surname, pesel, account_number, pin, balance)
+         VALUES (?, ?, ?, ?, ?, ?)",
+    )?;
+
+    for customer in customers {
+        statement.execute(&[
+            &customer.name,
+            &customer.surname,
+            &customer.pesel,
+            &customer.account_number,
+            &customer.pin,
+            &customer.balance.to_string(),
+        ])?;
     }
 
-    fn deposit(&mut self, amount: f64) {
-        self.balance += amount;
-    }
+    statement.finalize()?;
 
-    fn withdraw(&mut self, amount: f64) -> bool {
-        if self.balance >= amount {
-            self.balance -= amount;
-            return true;
-        }
-        print!("Nie masz pieniedzy byku");
-        false
-    }
-    fn get_info(&self) -> String {
-        format!(
-            "Imie: {}\nNazwisko: {}\nPESEL: {}\nNumer konta: {}\nSaldo: {}",
-            self.first_name, self.last_name, self.pesel, self.account_number, self.balance
-        )
-    }
-
-    fn get_balance(&self) -> f64 {
-        self.balance
-    }
+    tx.commit()?;
+    Ok(())
 }
 
-fn handle_client(mut stream: TcpStream, client: &Arc<Mutex<Client>>) -> io::Result<()> {
-    Ok(loop {
-        let mut input = [0; 512];
-        let bytes_read = stream.read(&mut input)?;
-        let input_str = std::str::from_utf8(&input[..bytes_read]).unwrap().trim();
+fn read_customers_from_database(db_path: &str) -> Result<Vec<Customer>> {
+    let conn = Connection::open(db_path)?;
 
+    let mut statement =
+        conn.prepare("SELECT name, surname, pesel, account_number, pin, balance FROM customers")?;
+    let customer_rows = statement.query_map([], |row| {
+        Ok(Customer {
+            name: row.get(0)?,
+            surname: row.get(1)?,
+            pesel: row.get(2)?,
+            account_number: row.get(3)?,
+            pin: row.get(4)?,
+            balance: row.get::<_, f64>(5)?,
+        })
+    })?;
+
+    let customers: Result<Vec<Customer>> = customer_rows.collect();
+    customers
+}
+
+fn find_account(account_number: &str, customers: &[Customer]) -> Option<usize> {
+    customers
+        .iter()
+        .position(|customer| customer.account_number == account_number)
+}
+
+fn handle_balance(customers: &Vec<Customer>, index: usize) -> String {
+    let customer = &customers[index];
+    format!("Balance: {:.2}", customer.balance)
+}
+
+fn handle_client(mut stream: TcpStream, customers: Arc<Mutex<Vec<Customer>>>) {
+    let mut buffer;
+
+    loop {
+        buffer = [0; 512];
+        let bytes_read = stream.read(&mut buffer).unwrap();
         if bytes_read == 0 {
-            return Ok(());
+            return;
         }
 
-        let mut client_ref = client.lock().unwrap();
+        let input_temp = String::from_utf8_lossy(&buffer[..]);
+        let input = input_temp.trim_end_matches('\0');
 
-        match input_str {
-            "clear" => {
-                let response = format!("\x1B[2J\x1B[1;1H");
-                stream.write_all(response.as_bytes())?;
-            }
-            "data" => {
-                let response = format!("Dane klienta: {}", client_ref.get_info());
-                stream.write_all(response.as_bytes())?;
-            }
-            "saldo" => {
-                let response = format!("Saldo klienta: {}", client_ref.get_balance());
-                stream.write_all(response.as_bytes())?;
-            }
-            cmd if cmd.starts_with("deposit ") => {
-                let amount_str = cmd.trim_start_matches("deposit ").trim();
-                let amount = match amount_str.parse::<f64>() {
-                    Ok(amount) => amount,
-                    Err(_) => {
-                        stream.write_all(b"Nieprawidlowa kwota\n")?;
-                        continue;
+        let parts = input.split_whitespace().collect::<Vec<_>>();
+
+        let command = parts.get(0).unwrap();
+        let account_number = parts.get(1).unwrap();
+
+        let response: String;
+
+        println!(
+            "Command: '{}', account number: {}",
+            command,
+            account_number.clone()
+        );
+        let mut customers = customers.lock().unwrap();
+
+        match find_account(account_number, customers.as_ref()) {
+            Some(index) => {
+                let customer = &mut customers[index];
+
+                response = match command.trim() {
+                    "balance" => {
+                        format!("Balance: {:.2}", customer.balance)
                     }
-                };
-                client_ref.deposit(amount);
-                stream.write_all(b"Wplata zrealizowana\n")?;
-            }
-            cmd if cmd.starts_with("withdraw ") => {
-                let amount_str = cmd.trim_start_matches("withdraw ").trim();
-                let ammount = match amount_str.parse::<f64>() {
-                    Ok(ammount) => ammount,
-                    Err(_) => {
-                        stream.write_all(b"Nieprawidlowa kwota\n")?;
-                        continue;
+                    "withdraw" => {
+                        let pin = parts.get(2).unwrap().to_string();
+                        let amount = parts.get(3).unwrap().to_string().parse::<f64>().unwrap();
+
+                        if customer.pin != pin {
+                            "Invalid PIN.".to_string()
+                        } else if customer.balance >= amount {
+                            customer.balance -= amount;
+                            format!("Success! New balance: {}", customer.balance)
+                        } else {
+                            "Insufficient funds.".to_string()
+                        }
                     }
+                    "deposit" => {
+                        let amount = parts.get(2).unwrap().to_string().parse::<f64>().unwrap();
+                        customer.balance += amount;
+                        format!("Success")
+                    }
+                    "transfer" => {
+                        let dest = parts.get(2).unwrap().to_string();
+                        let amount = parts.get(3).unwrap().to_string().parse::<f64>().unwrap();
+                        let pin = parts.get(4).unwrap().to_string();
+
+                        if customer.pin != pin {
+                            "Invalid PIN.".to_string()
+                        } else if customer.balance >= amount {
+                            if let Some(index2) = find_account(dest.as_str(), customers.as_ref()) {
+                                customers[index].balance -= amount;
+                                customers[index2].balance += amount;
+                                format!("Success! New balance: {}", customers[index].balance)
+                            } else {
+                                "Unknown destination account number".to_string()
+                            }
+                        } else {
+                            "Insufficient funds.".to_string()
+                        }
+                    }
+                    _ => "Invalid operation.".to_string(),
                 };
-                if client_ref.withdraw(ammount) {
-                    stream.write_all(b"Wyplata zrealizowana\n")?;
-                } else {
-                    stream.write_all(b"Brak wystarczajacych srodkow\n")?;
-                }
+                write_customers_to_database(customers.as_ref(), SAVE_PATH)
+                    .expect("Failed to save customers to file.");
             }
-            "exit" => {
-                break;
-            }
-            _ => {
-                stream.write_all(b"Nieznane polecenie\n")?;
+            None => {
+                response = "Unknown account number".to_string();
             }
         }
-    })
+
+        let _ = stream.write(response.as_bytes());
+    }
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    let client = Arc::new(Mutex::new(Client::new(
-        "John".to_string(),
-        "JOhny".to_string(),
-        "123421421".to_string(),
-        "1234".to_string(),
-        1000.0,
-    )));
+fn main() -> io::Result<()> {
+    let customers = Arc::new(Mutex::new(
+        read_customers_from_database(SAVE_PATH).expect("Failed to read customers from save file."),
+    ));
+
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    println!("Server listening on port 8080...");
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        let client = Arc::clone(&client);
-        thread::spawn(move || {
-            if let Err(e) = handle_client(stream, &client) {
-                eprintln!("Error occurred while handling client: {}", e);
+        match stream {
+            Ok(stream) => {
+                let customers = Arc::clone(&customers);
+                println!("New connection: {}", stream.peer_addr()?);
+                thread::spawn(move || handle_client(stream, customers));
             }
-        });
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
     }
+
+    Ok(())
 }
